108a45ed26b0c4b950d89eea2d4d3be24eef3fca
于汝漩 <yuruxuan@xijinweifu.onaliyun.com>
2019-03-26 16:32:40 +0800

    Add About & i18n

---
+++ app/src/main/java/coding/yu/smartcodescanner/ui/AboutActivity.java
@@ -0,0 +1,22 @@
+package coding.yu.smartcodescanner.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import coding.yu.smartcodescanner.R;
+
+public class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        findViewById(R.id.image_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/coding/yu/smartcodescanner/ui/MainActivity.java
+++ app/src/main/java/coding/yu/smartcodescanner/ui/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends BaseActivity {
         layoutAbout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
             }
         });
     }
